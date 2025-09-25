f8a84e6fa8b0f07a02fd1cbd424340ab80a1b344
kev <hopfkevin@yahoo.de>
2025-09-25 21:21:22 +0000

    Changes to Style and Function

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     window.location.href = 'menu.html';
 });
+const searchBox = document.getElementById('searchBox');
+const foodItems = document.querySelectorAll('.food-item li');
+
+searchBoxaddEventListener('input', function() {
+    const query = searchBox.ariaValueMax.toLowerCase();
+
+    foodItems.forEach(function(item) {
+        const text = item.textContent.toLowerCase();
+        if (text.includes(query)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
